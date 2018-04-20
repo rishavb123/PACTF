5d890595a560dfae080542d9f86861d64f225210
Rishav Bhagat <10013225@sbschools.org>
2018-04-20 09:19:44 -0400

    working on lottery

---
+++ lottery/test.py
@@ -0,0 +1,16 @@
+minimum = -2147483648 
+maximum = 2147483647
+guesses = [23588283, -5392995]
+answers = [-2069001995, -1997362081]
+
+print guesses[1] - guesses[0]
+print answers[1] - answers[0]
+
+print guesses[0] - answers[0]
+print guesses[1] - answers[1]
+
+print guesses[1] - answers[0]
+print answers[1] - guesses[0]
+
+def f(i):
+    if()
